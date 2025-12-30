2b52e4f0f1bae1d177353e26c9938a4ffc2cbf1c
redimrr-hue <redimrr@gmail.com>
2025-12-30 15:30:13 +0300

    7th Exercise

---
+++ Exercise_7.py
@@ -0,0 +1,24 @@
+#Loop 1
+print("Count up from 0 to 50:")
+for i in range(0, 51):
+    print(i)
+
+#Loop 2
+print("\nCount down from 50 to 0:")
+for i in range(50, -1, -1):
+    print(i)
+
+#Loop 3
+print("\nCount up from 30 to 50:")
+for i in range(30, 51):
+    print(i)
+
+#Loop 4
+print("\nCount down from 50 to 10 (by 2):")
+for i in range(50, 9, -2):
+    print(i)
+
+#Loop 5
+print("\nCount up from 100 to 200 (by 5):")
+for i in range(100, 201, 5):
+    print(i)
