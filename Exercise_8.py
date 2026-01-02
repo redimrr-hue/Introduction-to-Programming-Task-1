330732b308edd014bfbc1b9e01980dd20e1c8554
redimrr-hue <redimrr@gmail.com>
2026-01-02 13:44:40 +0300

    Updates

--- Exercise_8.py
+++ Exercise_8.py
@@ -4,5 +4,6 @@ search_name = input("Please search for a name on the list: ")
 
 if search_name in names:
     print(f"{search_name} was found in the list")
+    
 else:
     print(f"{search_name} was not found in the list")
